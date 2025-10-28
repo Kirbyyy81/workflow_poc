79627b428eee1bcde9d85612b3e066300c23df1f
Kirbyyy81 <ashleyc.work27@gmail.com>
2025-10-28 09:50:15 +0800

    feat: add AddIcon and CloseIcon components with SVG support

---
+++ src/components/assets/icons/index.js
@@ -0,0 +1,2 @@
+// export { ReactComponent as AddIcon } from "./add_icon.svg?react";
+// export { ReactComponent as CloseIcon } from "./close_icon.svg?react";
--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from '@tailwindcss/vite'
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
   plugins: [
+    svgr({
+      include: "**/*.svg",
+      enforce: "pre", // <--- critical line for Vite 7
+      svgrOptions: { icon: true },
+    }),
     react(), 
     tailwindcss()
   ],
